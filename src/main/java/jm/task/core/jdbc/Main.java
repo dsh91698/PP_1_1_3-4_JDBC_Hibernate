d562510b1e1f7456a14075d361f0c1ee5be59d0c
dsh91698 <mettus@mail.ru>
2023-05-20 03:24:59 +0400

    feat: in UserDaoJDBCImpl implement methods for creation and dropping table from database

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,15 +1,18 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.util.Util;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
         // реализуйте алгоритм здесь
+        UserDaoJDBCImpl userDaoJDBCImpl = new UserDaoJDBCImpl();
+            userDaoJDBCImpl.createUsersTable(); // create table if NOT exist
+            userDaoJDBCImpl.dropUsersTable(); // delete table if exist
 
-        Util.getConnection();
+        
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,7 +1,11 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -11,9 +15,34 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void createUsersTable() {
 
+        String createUserTableSql = "CREATE TABLE IF NOT EXISTS users ("
+                + "id BIGINT PRIMARY KEY,"
+                + "name VARCHAR(255),"
+                + "lastName VARCHAR(255),"
+                + "age TINYINT UNSIGNED"
+                + ")";
+
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement();) {
+             statement.execute(createUserTableSql); //create new table if not exist
+            System.out.println("Table created successfully.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void dropUsersTable() {
+        // SQL DROP TABLE statement
+        String dropTableSql = "DROP TABLE IF EXISTS users";
+
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement();) {
+            statement.executeUpdate(dropTableSql);
+            System.out.println("Table dropped successfully.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
