5549fb779aa2e751c37ed20c381311ea994ff003
dsh91698 <mettus@mail.ru>
2023-05-20 02:28:03 +0400

    feat: DB connection method add to Util - getConnection()

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,15 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // реализуйте алгоритм здесь
+
+        Util.getConnection();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,17 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private final static String URL = "jdbc:mysql://localhost:3306/kataTest";
+    private final static String USER = "root";
+    private final static String PASSWORD = "root";
+
+    public static Connection getConnection() throws SQLException {
+        System.out.println("getting connection to database ... ");
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
