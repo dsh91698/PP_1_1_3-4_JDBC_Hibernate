521006aa236ce8d572af18bbc9eb4f838c4d4160
dsh91698 <mettus@mail.ru>
2023-05-20 20:55:33 +0400

    feat: task done

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -11,6 +11,14 @@ public class Main {
         // реализуйте алгоритм здесь
         UserDaoJDBCImpl userDaoJDBCImpl = new UserDaoJDBCImpl();
             userDaoJDBCImpl.createUsersTable(); // create table if NOT exist
+            userDaoJDBCImpl.saveUser("Ivan", "Ivanov", (byte) 25);
+            userDaoJDBCImpl.saveUser("Petr", "Petrov", (byte) 26);
+            userDaoJDBCImpl.saveUser("Sidor", "Sidorov", (byte) 27);
+            userDaoJDBCImpl.saveUser("Egor", "Egorov", (byte) 28);
+            userDaoJDBCImpl.getAllUsers(); //get all users from DB and print it out
+            userDaoJDBCImpl.removeUserById(1); // delete used
+            userDaoJDBCImpl.cleanUsersTable();
+            userDaoJDBCImpl.getAllUsers(); //get all users from DB and print it out
             userDaoJDBCImpl.dropUsersTable(); // delete table if exist
 
         
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,9 +3,8 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
@@ -16,15 +15,15 @@ public class UserDaoJDBCImpl implements UserDao {
     public void createUsersTable() {
 
         String createUserTableSql = "CREATE TABLE IF NOT EXISTS users ("
-                + "id BIGINT PRIMARY KEY,"
+                + "id BIGINT AUTO_INCREMENT PRIMARY KEY,"//"id BIGINT PRIMARY KEY,"
                 + "name VARCHAR(255),"
                 + "lastName VARCHAR(255),"
                 + "age TINYINT UNSIGNED"
                 + ")";
 
         try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement();) {
-             statement.execute(createUserTableSql); //create new table if not exist
+             Statement statement = connection.createStatement()) {
+            statement.execute(createUserTableSql); //create new table if not exist
             System.out.println("Table created successfully.");
         } catch (SQLException e) {
             e.printStackTrace();
@@ -33,11 +32,10 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void dropUsersTable() {
-        // SQL DROP TABLE statement
         String dropTableSql = "DROP TABLE IF EXISTS users";
 
         try (Connection connection = Util.getConnection();
-             Statement statement = connection.createStatement();) {
+             Statement statement = connection.createStatement()) {
             statement.executeUpdate(dropTableSql);
             System.out.println("Table dropped successfully.");
         } catch (SQLException e) {
@@ -47,18 +45,107 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        String insertSql = "INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)";
 
+        try (Connection connection = Util.getConnection()) {
+
+            try (PreparedStatement stat = connection.prepareStatement(insertSql)) {
+                stat.setString(1, name);
+                stat.setString(2, lastName);
+                stat.setByte(3, age);
+                stat.executeUpdate();
+                System.out.println("User " + name + " added to database!");
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
 
+        String deleteUserSql = "DELETE FROM users WHERE id = ?";
+
+        try (Connection connection = Util.getConnection()) {
+
+            try (PreparedStatement stat = connection.prepareStatement(deleteUserSql)) {
+                stat.setLong(1, id);
+                int quantityDeleted = stat.executeUpdate();
+                if (quantityDeleted > 0) {
+                    System.out.println("User with ID " + id + " deleted from database!");
+                } else {
+                    System.out.println("User with ID " + id + " not found in database!");
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
     public List<User> getAllUsers() {
-        return null;
+        int usersPrintedOutCounter = 0;
+        List<User> userList = new ArrayList<>();
+        String allUsersSql = "SELECT * FROM users";
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement()) {
+            if (statement.execute(allUsersSql)) {
+                ResultSet resultSet = statement.getResultSet();
+                while (resultSet.next()) {
+                    long id = resultSet.getLong("id");
+                    String name = resultSet.getString("name");
+                    String lastName = resultSet.getString("lastName");
+                    byte age = resultSet.getByte("age");
+
+                    User user = new User(name, lastName, age);
+                    user.setId(id);
+                    userList.add(user);
+                    System.out.println(user);
+                    usersPrintedOutCounter++;
+                }
+            }
+
+            if (usersPrintedOutCounter > 0) {
+                System.out.println("Table printed out successfully");
+            } else {
+                System.out.println("No users for print in table");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return userList;
     }
 
     public void cleanUsersTable() {
+        String deleteAllsql = "DELETE FROM users";
+
+        try (Connection connection = Util.getConnection()) {
+
+            try (PreparedStatement stat = connection.prepareStatement(deleteAllsql)) {
+
+                int quantityDeleted = stat.executeUpdate();
+                if (quantityDeleted > 0) {
+                    System.out.println("All users deleted from database!");
+                } else {
+                    System.out.println("No Users deleted in database!");
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 }
