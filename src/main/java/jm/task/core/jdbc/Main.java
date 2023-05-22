a0fe5e7470d733c620dd7fd4ed6f7fa28a7eac3a
dsh91698 <mettus@mail.ru>
2023-05-22 19:24:16 +0400

    fix: minor fix

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,6 +1,10 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
 import java.sql.Connection;
@@ -9,17 +13,18 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException {
         // реализуйте алгоритм здесь
-        UserDaoJDBCImpl userDaoJDBCImpl = new UserDaoJDBCImpl();
-            userDaoJDBCImpl.createUsersTable(); // create table if NOT exist
-            userDaoJDBCImpl.saveUser("Ivan", "Ivanov", (byte) 25);
-            userDaoJDBCImpl.saveUser("Petr", "Petrov", (byte) 26);
-            userDaoJDBCImpl.saveUser("Sidor", "Sidorov", (byte) 27);
-            userDaoJDBCImpl.saveUser("Egor", "Egorov", (byte) 28);
-            userDaoJDBCImpl.getAllUsers(); //get all users from DB and print it out
-            userDaoJDBCImpl.removeUserById(1); // delete used
-            userDaoJDBCImpl.cleanUsersTable();
-            userDaoJDBCImpl.getAllUsers(); //get all users from DB and print it out
-            userDaoJDBCImpl.dropUsersTable(); // delete table if exist
+//        UserDao userService = new UserDaoJDBCImpl();
+        UserService userService = new UserServiceImpl();
+            userService.createUsersTable(); // create table if NOT exist
+            userService.saveUser("Ivan", "Ivanov", (byte) 25);
+            userService.saveUser("Petr", "Petrov", (byte) 26);
+            userService.saveUser("Sidor", "Sidorov", (byte) 27);
+            userService.saveUser("Egor", "Egorov", (byte) 28);
+            userService.getAllUsers(); //get all users from DB and print it out
+            userService.removeUserById(1); // delete user
+//            userService.cleanUsersTable();
+//            userService.getAllUsers(); //get all users from DB and print it out
+            userService.dropUsersTable(); // delete table if exist
 
         
     }
