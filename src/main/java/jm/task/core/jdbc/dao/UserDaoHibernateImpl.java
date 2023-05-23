a2505d5428dd52764a618b790e94eb7514567668
dsh91698 <mettus@mail.ru>
2023-05-23 14:08:46 +0400

    feat: transaction rollbacks add into userDaoHibernateImpl

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -3,10 +3,8 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
@@ -18,8 +16,9 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public void createUsersTable() {
 
+        Transaction transaction = null;
         try (Session session = Util.getSession()) {
-            session.beginTransaction();
+            transaction = session.beginTransaction();
             session.createSQLQuery("CREATE TABLE IF NOT EXISTS users (" +
                     "id BIGINT AUTO_INCREMENT," +
 //                    "id BIGINT PRIMARY KEY, " +
@@ -27,11 +26,15 @@ public class UserDaoHibernateImpl implements UserDao {
                     "lastName VARCHAR(255), " +
                     "age TINYINT UNSIGNED, " +
                     "PRIMARY KEY (id))").executeUpdate();
-            session.getTransaction().commit();
+            transaction.commit();
+//            session.getTransaction().commit();
             System.out.println("New table created in database!");
 
         } catch (Exception e) {
             e.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();// Roll back the transaction
+            }
         }
 
 
@@ -39,16 +42,21 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void dropUsersTable() {
+        Transaction transaction = null;
 
         String dropTableSql = "DROP TABLE IF EXISTS users";
         try (Session session = Util.getSession()) {
-            session.beginTransaction();
+            transaction = session.beginTransaction();
             session.createSQLQuery(dropTableSql).executeUpdate();
-            session.getTransaction().commit();
+            transaction.commit();
             System.out.println("Table dropped (removed from database)!");
 
         } catch (Exception e) {
             e.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();// Roll back the transaction
+            }
+
         }
 
 
@@ -56,21 +64,29 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        Transaction transaction = null;
+
         try (Session session = Util.getSession()) {
             User user = new User(name, lastName, age);
-            session.beginTransaction();
+            transaction = session.beginTransaction();
             session.save(user);
-            session.getTransaction().commit();
+            transaction.commit();
             System.out.println("user id -> " + user.getId());
         } catch (Exception e) {
             e.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();// Roll back the transaction
+            }
+
         }
     }
 
     @Override
     public void removeUserById(long id) {
+        Transaction transaction = null;
+
         try (Session session = Util.getSession()) {
-            session.beginTransaction();
+            transaction = session.beginTransaction();
             User user = session.get(User.class, id);
             if (user != null) {
                 session.delete(user);// User found, delete the user
@@ -78,42 +94,56 @@ public class UserDaoHibernateImpl implements UserDao {
             } else {
                 System.out.println("User with ID " + id + " do not exist in database!");// User not found
             }
-            session.getTransaction().commit();
+            transaction.commit();
         } catch (Exception e) {
             e.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();// Roll back the transaction
+            }
         }
 
     }
 
     @Override
     public List<User> getAllUsers() {
+        Transaction transaction = null;
+
         List<User> userList = null;
 
         try (Session session = Util.getSession()) {
-            session.beginTransaction();
+            transaction = session.beginTransaction();
             userList = session.createQuery("from User").getResultList();
-            session.getTransaction().commit();
+            transaction.commit();
 
             for (User user : userList) {
                 System.out.println(user);
             }
         } catch (Exception e) {
             e.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();// Roll back the transaction
+            }
+
         }
         return userList;
     }
 
     @Override
     public void cleanUsersTable() {
+        Transaction transaction = null;
 
         try (Session session = Util.getSession()) {
-            session.beginTransaction();
+            transaction = session.beginTransaction();
             session.createQuery("delete User").executeUpdate();
-            session.getTransaction().commit();
+            transaction.commit();
             System.out.println("All users deleted from the database!");
 
         } catch (Exception e) {
             e.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();// Roll back the transaction
+            }
+
         }
 
 
