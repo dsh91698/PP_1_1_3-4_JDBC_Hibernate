bec1a884c65daecb05eeefaae48e45775a00f963
dsh91698 <mettus@mail.ru>
2023-05-23 14:07:40 +0400

    refactor: variables used in Util in Hibernate settings

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -22,6 +22,8 @@ public class Util {
     private final static String PASSWORD = "root";
 
     // ----------- hibernate ---------------------------------------------------------------------------------
+    private final static String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private final static String DIALECT = "org.hibernate.dialect.MySQL5Dialect";
     private static SessionFactory sessionFactory;
 
     public static SessionFactory getSessionFactory() {
@@ -31,11 +33,11 @@ public class Util {
 
                 // Hibernate settings equivalent to hibernate.cfg.xml's properties
                 Properties settings = new Properties();
-                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.DRIVER, DRIVER);
                 settings.put(Environment.URL, URL + "?useSSL=false");
-                settings.put(Environment.USER, "root");
-                settings.put(Environment.PASS, "root");
-                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+                settings.put(Environment.USER, USER);
+                settings.put(Environment.PASS, PASSWORD);
+                settings.put(Environment.DIALECT, DIALECT);
 
                 settings.put(Environment.SHOW_SQL, "true");
 
